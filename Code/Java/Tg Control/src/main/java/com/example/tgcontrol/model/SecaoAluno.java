645d7952a3ddb8c1f069fa7cdd406c3ac27ca3fa
Leonardo1022 <leonardo.amon.sumiyoshi@gmail.com>
2025-11-16 23:08:27 -0300

    update(tudo): atualizado a branch

--- Code/Java/Tg Control/src/main/java/com/example/tgcontrol/model/SecaoAluno.java
+++ Code/Java/Tg Control/src/main/java/com/example/tgcontrol/model/SecaoAluno.java
@@ -7,18 +7,15 @@ public class SecaoAluno {
     private final String emailAluno;
     private final int taskSequence;
     private final String titulo;
-    private final String status;
-    private final LocalDate dataEntrega;
-    private final String statusRevisao;
     private final LocalDateTime dataUltimaRevisao;
     private final String status; // Status da TAREFA: "Concluído", "Em Progresso", "Bloqueado"
     private final LocalDate dataEntrega; //Até os minutos
     private final String statusRevisao; // Status da REVISÃO: "Aprovado", "Revisão Solicitada", "Pendente"
-    //private final String status;
-    //private final LocalDate dataEntrega;
-    //private final String statusRevisao;
 
-    public SecaoAluno(String emailAluno, int taskSequence, String titulo, String status, LocalDate dataEntrega, String statusRevisao, LocalDateTime dataUltimaRevisao) {
+    public SecaoAluno(
+            String emailAluno, int taskSequence, String titulo, String status,
+            LocalDate dataEntrega, String statusRevisao, LocalDateTime dataUltimaRevisao
+    ) {
         this.emailAluno = emailAluno;
         this.taskSequence = taskSequence;
         this.titulo = titulo;
